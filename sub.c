6592152f15a759cff6d2febb3160b0cf2d3b2a8c
udohchuks <urichard132@gmail.com>
2023-06-22 08:53:18 +0100

    sub

---
+++ sub.c
@@ -0,0 +1,20 @@
+#include "monty.h"
+
+/**
+ * sub - add top 2 node
+ * @stack: stack
+ * @line_number: line number
+ */
+
+void sub(stack_t **stack, unsigned int line_number)
+{
+    if (*stack == NULL || (*stack)->next == NULL)
+    {
+        fprintf(stderr, "L%d: can't sub, stack too short\n", line_number);
+	free_stack(*stack);
+        exit(EXIT_FAILURE);
+    }
+
+    (*stack)->next->n -= (*stack)->n;
+    pop(stack, line_number);
+}
