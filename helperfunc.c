de2c51634c68c715a649342cbbb08196152c6bee
udohchuks <urichard132@gmail.com>
2023-06-22 03:06:21 +0100

    nop

--- helperfunc.c
+++ helperfunc.c
@@ -15,6 +15,7 @@ void (*op_func(char *opcode))(stack_t **stack, unsigned int line_number)
 		{"pop", pop},
 		{"swap", swap},
 		{"add", add},
+		{"nop", nop},
 		{NULL, NULL}
 	};
 	int i = 0;
---
+++ nop.c
@@ -0,0 +1,14 @@
+#include "monty.h"
+
+/**
+ * nop - nop
+ * @stack: stack
+ * @line_number: line_number
+ */
+
+void nop(stack_t **stack, unsigned int line_number)
+{
+    (void)stack;
+    (void)line_number;
+}
+
